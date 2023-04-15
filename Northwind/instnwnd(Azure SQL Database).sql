fd8d51da48f0838e616dc0adf7a09323e4ef0d8c
Mehmet Can Çetin <92088210+mcctnn@users.noreply.github.com>
2023-04-15 11:21:22 +0300

    Update instnwnd(Azure SQL Database).sql

--- Northwind/instnwnd(Azure SQL Database).sql
+++ Northwind/instnwnd(Azure SQL Database).sql
@@ -1,12 +1,3 @@
-/*
-** Copyright Microsoft, Inc. 1994 - 2000
-** All Rights Reserved.
-*/
-
--- This script does not create a database.
--- Run this script in the database you want the objects to be created.
--- Default schema is dbo.
-
 SET NOCOUNT ON
 GO
 
